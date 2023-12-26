0098d66cd6b4386241651ccab342c4dcd8b54da1
Ajxd9 <134958823+Ajxd9@users.noreply.github.com>
2023-12-27 01:38:57 +0200

    aa

--- script/projects.js
+++ script/projects.js
@@ -78,7 +78,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     card.addEventListener("click", () => {
       // Navigate to projectPage.html with card name as a parameter
-      window.location.href = `projectPage.html?projectName=${encodeURIComponent(
+      window.location.href = `./projectPage.html?projectName=${encodeURIComponent(
         project.projectName
       )}`;
     });
